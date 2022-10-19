eb802fa1eb05756639d3cc5af247d06dc8e46438
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-19 06:21:21 +0100

    code update

--- src/pages/Homepage/BespokePlans/BespokePlans.component.jsx
+++ src/pages/Homepage/BespokePlans/BespokePlans.component.jsx
@@ -2,8 +2,28 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import './BespokePlans.styles.scss';
 
+const plans = [
+  {
+    title: 'Comprehensive',
+    body: 'Covers all the vehicle used to drive growth in your business',
+  },
+  {
+    title: 'Monthly Comp. Auto',
+    body: 'Minimise vehicle risks with an affordable auto insurance.',
+  },
+  {
+    title: 'Mini Comp. Auto',
+    body: 'Get your vehicles on the road without worry on your budget.',
+  },
+  {
+    title: '3rd Party Auto',
+    body: 'Drive with confidence while you save on insurance costs.',
+  },
+];
+
 const BespokePlans = () => {
   const [activeTab, setActiveTab] = useState('Auto');
+
   return (
     <div className='bespoke-plans'>
       <p className='bespoke-plans-intro'>BespokePlans</p>
@@ -20,37 +40,49 @@ const BespokePlans = () => {
             key={item}
             variant={activeTab === item ? 'primary-green' : 'gray-100'}
             onClick={() => setActiveTab(item)}
+            className={`${
+              activeTab === item ? 'text-white' : 'text-primaryBlack'
+            } px-md-3 action-btn`}
           >
             {item}
           </Button>
         ))}
       </div>
       <div className='bespoke-plans-content-display'>
-        <div className='bespoke-plans-content-display-auto'>
-          {[0, 1, 2, 3].map((auto) => (
-            <div
-              key={auto}
-              className='bespoke-plans-content-display-auto-card'
-            ></div>
-          ))}
-        </div>
-        <div className='bespoke-plans-content-display-others'>
-          <img
-            src='../../../assets/svg/auto.svg'
-            alt='auto'
-            className='bespoke-plans-content-display-others-icon'
-          />{' '}
-          <span className='bespoke-plans-content-display-others-title'>
-            Comprehensive
-          </span>
-          <div className='bespoke-plans-content-display-others-body'>
-            Covers all the vehicle used to drive growth in your business
+        {activeTab === 'Auto' ? (
+          <div className='bespoke-plans-content-display-auto'>
+            {plans.map(({ title, body }) => (
+              <div
+                key={title}
+                className='bespoke-plans-content-display-auto-card'
+              >
+                <img
+                  src='/auto.svg'
+                  alt='auto'
+                  className='bespoke-plans-content-display-auto-card-icon'
+                />
+                <span className='bespoke-plans-content-display-auto-card-title'>
+                  {title}
+                </span>
+                <p className='bespoke-plans-content-display-auto-card-body'>
+                  {body}
+                </p>
+                <p className='bespoke-plans-content-display-auto-card-link'>
+                  Learn More
+                  <img
+                    src='/arrow-right.svg'
+                    alt='arrow right'
+                    className='right-arrow-icon'
+                  />
+                </p>
+              </div>
+            ))}
           </div>
-          <p className='bespoke-plans-content-display-others-link'>
-            Learn More
-            <img src='../../../assets/svg/arrow-right.svg' alt='arrow right' />
-          </p>
-        </div>
+        ) : (
+          <div className='bespoke-plans-content-display-others'>
+            <h4>STILL UNDER DEVELOPMENT</h4>
+          </div>
+        )}
       </div>
     </div>
   );
