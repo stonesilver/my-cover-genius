57a5f01f22342e47a1fdd1c8d398ac5a720f55a1
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-20 22:23:00 +0100

    better file architecture

--- src/pages/Homepage/Homepage.page.jsx
+++ src/pages/Homepage/Homepage.page.jsx
@@ -1,12 +1,12 @@
-import AllRoundInsurance from './AllRoundInsurance/AllRoundInsurance.component';
-import Banner from './Banner/Banner.component';
-import BespokePlans from './BespokePlans/BespokePlans.component';
-import BespokeSolutions from './BespokeSolutions/BespokeSolutions.component';
-import Brands from './Brands/Brands.component';
-import ClaimsAssessments from './ClaimsAssessments/ClaimsAssessments.component';
-import CoCreatingSolution from './CoCreatingSolution/CoCreatingSolution.component';
-import Faq from './Faq/Faq.component';
-import GetInsurance from './GetInsurance/GetInsurance.component';
+import AllRoundInsurance from '../../components/Homepage/AllRoundInsurance/AllRoundInsurance.component';
+import Banner from '../../components/Homepage/Banner/Banner.component';
+import BespokePlans from '../../components/Homepage/BespokePlans/BespokePlans.component';
+import BespokeSolutions from '../../components/Homepage/BespokeSolutions/BespokeSolutions.component';
+import Brands from '../../components/Homepage/Brands/Brands.component';
+import ClaimsAssessments from '../../components/Homepage/ClaimsAssessments/ClaimsAssessments.component';
+import CoCreatingSolution from '../../components/Homepage/CoCreatingSolution/CoCreatingSolution.component';
+import Faq from '../../components/Homepage/Faq/Faq.component';
+import GetInsurance from '../../components/Homepage/GetInsurance/GetInsurance.component';
 
 const Homepage = () => {
   return (
