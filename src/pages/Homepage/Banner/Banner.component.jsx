acfe50d11d7b1ea0bb59efda78ccb15c11b140a6
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-20 08:09:06 +0100

    code update

--- src/pages/Homepage/Banner/Banner.component.jsx
+++ src/pages/Homepage/Banner/Banner.component.jsx
@@ -1,4 +1,10 @@
 import Button from 'react-bootstrap/Button';
+import { ReactComponent as GadgetIcon } from '../../../assets/svg/gadget.svg';
+import { ReactComponent as AutoIcon } from '../../../assets/svg/auto.svg';
+import { ReactComponent as HealthIcon } from '../../../assets/svg/health-cover.svg';
+import { ReactComponent as LogisticsIcon } from '../../../assets/svg/logistics-cover.svg';
+import { ReactComponent as OfficeContentIcon } from '../../../assets/svg/office-content.svg';
+import { ReactComponent as TravelIcon } from '../../../assets/svg/travel-cover.svg';
 import Col from 'react-bootstrap/Col';
 import './Banner.styles.scss';
 
@@ -8,8 +14,8 @@ const Banner = () => {
       <div className='row'>
         <Col xs={12} lg={7} className='banner-text'>
           <p className='bold-text'>
-            Set your <span className='special-text'>business</span> up for success with
-            seamless insurance
+            Set your <span className='special-text'>business</span> up for
+            success with seamless insurance
           </p>
           <p className='sub-text'>
             We protect all your hard work with affordable insurance to help your
@@ -23,11 +29,30 @@ const Banner = () => {
           <div className='animation-container'>
             <div className='center-image-container'>
               <img
-                src='/img/Mask-group.webp'
+                src='/img/services/Mask-group.webp'
                 alt='center'
                 className='center-image'
               />
               <div className='indicator'></div>
+            </div>
+            <div className='service-box gadget-cover'>
+              <GadgetIcon className='gadget-icon' />
+              <p className="gadget-cover-text">Gadget Cover</p>
+            </div>
+            <div className='service-box health-cover'>
+              <HealthIcon className='health-icon' />
+            </div>
+            <div className='service-box logistic-cover'>
+              <LogisticsIcon className='logitics-icon' />
+            </div>
+            <div className='service-box travel-cover'>
+              <TravelIcon className='travel-icon' />
+            </div>
+            <div className='service-box office-content'>
+              <OfficeContentIcon className='office-icon' />
+            </div>
+            <div className='service-box auto'>
+              <AutoIcon className='auto-icon' />
             </div>
           </div>
         </Col>
